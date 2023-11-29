1e52022264d1b28e36c24a09bdd8e1258768b78b
Shmagi97 <135329382+Shmagi97@users.noreply.github.com>
2023-11-29 19:19:54 +0400

    end
    end

--- lesson-6/src/App.jsx
+++ lesson-6/src/App.jsx
@@ -1,49 +1,69 @@
 import { useEffect, useState } from 'react'
 import YutubeCo from './yotubeKomponent'
+// import "./App.css"
 
-// import './App.css'
+import styled from 'styled-components'
 
-const StyledSection = styled.main`
-  
-  width: 90%;
 
-  border: solid red;
-  margin: 0 auto;
-  display: flex;
-  column-gap: 30px;
-  `;
 
 function App() {
 
-  
+const Ragaca = styled.div`
+
+border: solid red;
+
+`
+
+const StyledComponent = styled.main`
+
+
+  width: 90%;
+
+ 
+  margin: 0 auto;
+  display: flex;
+  column-gap: 30px;
+
+
+`
+
+const IframeComponent = styled.iframe`
+
+width: 700px;
+height: 400px;
+border-radius: 4%;
+
+`
+
+const a = 10;
 
 return(
   // <main className='main'>
-  //   <div className='mainDiv1'>
-  //   <iframe className='iframe'
-  //    src="https://www.youtube.com/embed/Q0AVb8_lrgQ?si=egqulI4OesFigsvt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+  <StyledComponent>
+       <div style={{ background:  a == 10 ? "red" : "blue" }}>
+    <IframeComponent src="https://www.youtube.com/embed/Q0AVb8_lrgQ?si=egqulI4OesFigsvt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen >
 
-  //  </iframe>
-  //   </div>
+    </IframeComponent>
+    
+
+   
+    </div>
+
+    <YutubeCo/>
+  </StyledComponent>
+   
 
     
-  //     <YutubeCo/>
+      
     
    
   // </main>
 
-  <StyledSection>
 
-     <div className='mainDiv1'>
-       <iframe className='iframe'
-         src="https://www.youtube.com/embed/Q0AVb8_lrgQ?si=egqulI4OesFigsvt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
 
-       </iframe>
-     </div>
-
-    
-     <YutubeCo/>
-  </StyledSection>
+  
+   
+  
   
 )
  
